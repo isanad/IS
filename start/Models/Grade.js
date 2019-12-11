11b5909c40140dcc865ac0a6d1e560a968ef82e3
isanad <islam.sanad@student.guc.edu.eg>
2019-12-11 17:44:47 +0200

    unique user and grade addition

--- start/Models/Grade.js
+++ start/Models/Grade.js
@@ -4,7 +4,9 @@ const Schema = mongoose.Schema
 const GradeSchema = new Schema({
   subject: { type: String, required: true},
   grade: { type: String, required: true},
-  
+  user_id: {type: Number, required: true}
 })
+
+GradeSchema.index({ subject: 1, user_id: 1 }, { unique: true });
 
 module.exports = Grade = mongoose.model('grades', GradeSchema)
