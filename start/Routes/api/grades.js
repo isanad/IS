71516410abce3f14237159dfdf536285795a2ae2
isanad <islam.sanad@student.guc.edu.eg>
2019-12-11 23:08:17 +0200

    working on grade validation, fixed file name

--- start/Routes/api/grades.js
+++ start/Routes/api/grades.js
@@ -1,27 +1,31 @@
-const express = require('express')
-const router = express.Router()
-//const joi = require('joi')
-const Grade = require('../../Models/Grade')
-//const validator = require('../../Validation/eventValid')
+const express = require("express");
+const router = express.Router();
+const Grade = require("../../Models/Grade");
+const validator = require("../../Validation/gradeValid");
 
-  //post a grade
- router.post("/", async (req, res) => {
-    try {
-       const grade = await new Grade ({
-        subject: req.body.subject,
-        grade: req.body.grade,
-        user_id: req.body.user_id
-      }).save()
-   return res.json({ data: grade })
-    }catch (error) {
-      // We will be handling the error later
-      console.log(error)
+//post a grade
+router.post("/", async (req, res) => {
+  try {
+    const isValidated = validator.createValidation(req.body);
+    if (isValidated.error) {
+      return res
+        .status(400)
+        .send({ error: isValidated.error.details[0].message });
     }
-})
+    const grade = await new Grade({
+      subject: req.body.subject,
+      grade: req.body.grade,
+      user_id: req.body.user_id
+    }).save();
+    return res.json({ data: grade });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
- router.get("/", async (req, res) => {
-   const grades = await Grade.find();
-   res.json({ data: grades });
- });
+router.get("/", async (req, res) => {
+  const grades = await Grade.find();
+  res.json({ data: grades });
+});
 
-  module.exports = router;
+module.exports = router;
--- start/Routes/api/users.js
+++ start/Routes/api/users.js
@@ -1,37 +1,35 @@
-const express = require('express')
-const router = express.Router()
-//const joi = require('joi')
-const User = require('../../Models/User')
-const validator = require('../../Validation/userValid')
+const express = require("express");
+const router = express.Router();
+const User = require("../../Models/User");
+const validator = require("../../Validation/userValid");
 
-  //post a user
- router.post("/", async (req, res) => {
-    try {
-      const isValidated = validator.createValidation(req.body)
-      if (isValidated.error) {
-        return res
-          .status(400)
-          .send({ error: isValidated.error.details[0].message })
-      }
-       const user = await new User ({
-        name: req.body.name,
-        email: req.body.email,
-        username: req.body.username,
-        password: req.body.password,
-        age: req.body.age,
-        phoneNumber: req.body.phoneNumber,
-        userType: req.body.userType
-      }).save()
-   return res.json({ data: user })
-    }catch (error) {
-      // We will be handling the error later
-      console.log(error)
+//post a user
+router.post("/", async (req, res) => {
+  try {
+    const isValidated = validator.createValidation(req.body);
+    if (isValidated.error) {
+      return res
+        .status(400)
+        .send({ error: isValidated.error.details[0].message });
     }
-})
+    const user = await new User({
+      name: req.body.name,
+      email: req.body.email,
+      username: req.body.username,
+      password: req.body.password,
+      age: req.body.age,
+      phoneNumber: req.body.phoneNumber,
+      userType: req.body.userType
+    }).save();
+    return res.json({ data: user });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
- router.get("/", async (req, res) => {
-   const users = await User.find();
-   res.json({ data: users });
- });
+router.get("/", async (req, res) => {
+  const users = await User.find();
+  res.json({ data: users });
+});
 
-  module.exports = router;
+module.exports = router;
