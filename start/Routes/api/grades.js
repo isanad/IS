fdeecfa3edbb80f87db99cf4deee74b531a3d218
isanad <islam.sanad@student.guc.edu.eg>
2019-12-11 17:34:25 +0200

    mongo is connected, post works, routing correct

--- start/Routes/api/grades.js
+++ start/Routes/api/grades.js
@@ -1,29 +1,26 @@
 const express = require('express')
 const router = express.Router()
-const app = express()
 //const joi = require('joi')
 const Grade = require('../../Models/Grade')
 //const validator = require('../../Validation/eventValid')
 
   //post a grade
-  router.post("/", async (req, res) => {
-   /* try {
-      const isValidated = validator.createValidation(req.body)
-      if (isValidated.error) {
-        return res
-          .status(400)
-          .send({ error: isValidated.error.details[0].message })
-      }*/
-
-      const grade = await new Grade ({
+ router.post("/", async (req, res) => {
+    try {
+       const grade = await new Grade ({
         subject: req.body.subject,
         grade: req.body.grade
       }).save()
-
-    return res.json({ data: grade })
+   return res.json({ data: grade })
+    }catch (error) {
+      // We will be handling the error later
+      console.log(error)
+    }
 })
-app.get("/",  (req, res) => {
-  res.send(`<h1>Welcome</h1>`);
-});
+
+ router.get("/", async (req, res) => {
+   const grades = await Grade.find();
+   res.json({ data: grades });
+ });
 
   module.exports = router;
